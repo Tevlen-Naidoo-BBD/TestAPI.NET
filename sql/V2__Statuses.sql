1db3cf6a26d37e2f39892c34173419ef7f0ec2fc
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-06-21 13:57:49 +0200

    Getting started

---
+++ sql/V2__Statuses.sql
@@ -0,0 +1,13 @@
+INSERT INTO pet_statuses (pet_status_name)
+VALUES
+    ('HEALTHY'),
+    ('SICK'),
+    ('GERIATRIC'),
+    ('ADOPTED');
+
+INSERT INTO order_statuses (order_status_name)
+VALUES
+    ('IN PROGRESS'),
+    ('ACCEPTED'),
+    ('CANCELLED'),
+    ('REJECTED');
