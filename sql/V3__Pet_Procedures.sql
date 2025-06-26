b84cc87ef0d239095dc48cc6ff2bde42473745cf
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-06-26 11:06:32 +0200

    Initial functions and procedures

--- sql/V3__Pet_Procedures.sql
+++ sql/V3__Pet_Procedures.sql
@@ -1,15 +1,15 @@
-CREATE OR REPLACE IF EXISTS PROCEDURE add_pet (
-    p_pet_name VARCHAR(32),
-    p_pet_status INT DEFAULT 1,
-    p_pet_store INT DEFAULT NULL,
-    p_pet_image BYTEA DEFAULT NULL
+CREATE OR REPLACE PROCEDURE add_pet (
+   p_pet_name VARCHAR(32),
+   p_pet_status INT DEFAULT 1,
+   p_pet_store INT DEFAULT NULL,
+   p_pet_image BYTEA DEFAULT NULL
 ) LANGUAGE plpgsql AS $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pet_statuses WHERE pet_status_id = p_pet_status) THEN
         RAISE EXCEPTION 'Pet Status % does not exist', p_pet_status;
     END IF;
 
-    IF p_pet_store IS NOT NULL AND NOT EXISTS (SELECT 1 FROM stores WHERE store_id = p_pet_store) THEN
+    IF (p_pet_store IS NOT NULL) AND (NOT EXISTS (SELECT 1 FROM stores WHERE store_id = p_pet_store)) THEN
         RAISE EXCEPTION 'Pet Store % does not exist', p_pet_status;
     END IF;
 
@@ -20,20 +20,20 @@ BEGIN
 
     EXCEPTION
         WHEN others THEN
-            RAISE EXCEPTION 'Error adding pet: %', SQLERR;
+            RAISE EXCEPTION 'Error adding pet: %', SQLERRM;
 END;
 $$;
 
-CREATE OR REPLACE IF EXISTS PROCEDURE update_pet (
+CREATE OR REPLACE PROCEDURE update_pet (
     p_pet_id INT,
-    p_pet_name VARCHAR(32),
+    p_pet_name VARCHAR(32) DEFAULT NULL,
     p_pet_status INT DEFAULT NULL,
     p_pet_store INT DEFAULT NULL,
     p_pet_image BYTEA DEFAULT NULL,
     p_removed_at TIMESTAMPTZ DEFAULT NULL
 ) LANGUAGE plpgsql AS $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pet_statuses WHERE pet_status_id = p_pet_status) THEN
+    IF p_pet_status IS NOT NULL AND NOT EXISTS (SELECT 1 FROM pet_statuses WHERE pet_status_id = p_pet_status) THEN
         RAISE EXCEPTION 'Pet Status % does not exist', p_pet_status;
     END IF;
 
@@ -51,7 +51,7 @@ BEGIN
         pet_store = COALESCE(p_pet_store, pet_store),
         pet_status = COALESCE(p_pet_status, pet_status),
         pet_image = COALESCE(p_pet_image, pet_image),
-        removed_at = COALESCE(p_removed_at, removed_at),
+        removed_at = COALESCE(p_removed_at, removed_at)
     WHERE
         pet_id = p_pet_id;
 
@@ -59,6 +59,55 @@ BEGIN
 
     EXCEPTION
         WHEN others THEN
-            RAISE EXCEPTION 'Error updating pet: %', SQLERR;
+            RAISE EXCEPTION 'Error updating pet: %', SQLERRM;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE remove_pet (
+    p_pet_id INT
+) LANGUAGE plpgsql AS $$
+DECLARE
+    v_removed_at TIMESTAMPTZ;
+BEGIN
+
+    IF NOT EXISTS (SELECT 1 FROM pets wHERE pet_id = p_pet_id) THEN
+        RAISE EXCEPTION 'Pet ID % does not exist', p_pet_id;
+    END IF;
+
+    SELECT removed_at INTO v_removed_at FROM pets WHERE pet_id = p_pet_id;
+
+    IF v_removed_at IS NOT NULL THEN
+        RAISE EXCEPTION 'This pet has already been removed';
+    END IF;
+
+    CALL update_pet(p_pet_id, p_removed_at := NOW());
+
+    RAISE NOTICE 'Successfully removed pet';
+
+    EXCEPTION
+        WHEN others THEN
+            RAISE EXCEPTION 'Error removing pet: %', SQLERRM;
+
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE upload_pet_image (
+    p_pet_id INT,
+    p_pet_image BYTEA
+) LANGUAGE plpgsql AS $$
+BEGIN
+
+    IF NOT EXISTS (SELECT 1 FROM pets WHERE pet_id = p_pet_id) THEN
+        RAISE EXCEPTION 'Pet ID % does not exist', p_pet_id;
+    END IF;
+
+    CALL update_pet(p_pet_id, p_pet_image := p_pet_image);
+
+    RAISE NOTICE 'Successfully uploaded pet image';
+
+    EXCEPTION
+        WHEN others THEN
+            RAISE EXCEPTION 'Error uploading pet image: %', SQLERRM;
+            
 END;
 $$;
--- sql/V4__Pet_Functions.sql
+++ sql/V4__Pet_Functions.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE IF EXISTS FIND_PET (p_pet_id INT)
+CREATE OR REPLACE FUNCTION find_pet (p_pet_id INT)
 RETURNS TABLE (
   pet_id INT,
   pet_name VARCHAR(32),
@@ -8,7 +8,7 @@ RETURNS TABLE (
   removed_at TIMESTAMPTZ
 ) AS $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pets wHERE pet_id = p_pet_id) THEN
+    IF NOT EXISTS (SELECT 1 FROM pets WHERE pet_id = p_pet_id) THEN
         RAISE EXCEPTION 'Pet ID % does not exist', p_pet_id;
     END IF;
     
@@ -23,5 +23,34 @@ BEGIN
     INNER JOIN pet_statuses ps ON p.pet_status = ps.pet_status_id
     INNER JOIN pet_storees ps ON p.pet_store = ps.pet_store_id
     WHERE p.pet_id = p_pet_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION find_pets_by_status (p_pet_status INT)
+RETURNS TABLE (
+  pet_id INT,
+  pet_name VARCHAR(32),
+  pet_status INT,
+  pet_store INT,
+  pet_image BYTEA,
+  removed_at TIMESTAMPTZ
+) AS $$
+BEGIN
+
+    IF NOT EXISTS (SELECT 1 FROM pet_statuses WHERE pet_status_id = p_pet_status) THEN
+        RAISE EXCEPTION 'Pet Status % does not exist', p_pet_status;
+    END IF;
+    
+    RETURN QUERY
+    SELECT
+        p.pet_id,
+        p.pet_name,
+        ps.pet_status_name AS pet_status,
+        pst.pet_store_name AS pet_store,
+        p.pet_image
+    FROM pets p
+    INNER JOIN pet_statuses ps ON p.pet_status = ps.pet_status_id
+    INNER JOIN pet_storees ps ON p.pet_store = ps.pet_store_id
+    WHERE p.pet_status = p_pet_status;
 END;
 $$ LANGUAGE plpgsql;
