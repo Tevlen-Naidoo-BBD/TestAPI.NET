d190c6ca302296052d408f20c37795ee5120eb39
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-06-27 10:45:00 +0200

    Started the ASP.NET project
    Signed-off-by: Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
    Signed by Tevlen Naidoo <Tevlen.Naidoo@bbd.co.za>

---
+++ PetStoreAPI/Program.cs
@@ -0,0 +1,62 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Pet Store API",
+        Version = "v1",
+        Description = "A RESTful API for managing a pet store.",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Tevlen Naidoo",
+            Email = "tevlen.naidoo@gmail.com"
+        }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
